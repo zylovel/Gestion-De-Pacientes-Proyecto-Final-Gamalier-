d0c7e1a1a4c48db7d9a3375d69cbb2f430fa4be5
JohnDev <155394271+zylovel@users.noreply.github.com>
2026-08-12 22:56:08 -0400

    Add files via upload

---
+++ FinalGamalier/Pacientes/Pacientes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalGamalier.NewFolder.Pacientes
+{
+    public class Pacientes
+    {
+        public enum Sexo
+        {
+            Masculino,
+            Femenino
+        }
+        public enum EstadoPaciente
+        {
+            Ingresado,
+            EnObservacion,
+            DeAlta,
+            Hospitalizado
+        }
+        public class Paciente
+        {
+            public string Id { get; set; }
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+            public Sexo Sexo { get; set; }
+            public string Diagnostico { get; set; }
+            public EstadoPaciente Estado { get; set; }
+            public DateTime FechaIngreso { get; set; }
+            public Paciente() { FechaIngreso = DateAndTime.Now; }
+            public override string ToString()
+            {
+                return $"{Id} - {Nombre} ({Edad}Años)";
+            }
+        }
+    }
+}
